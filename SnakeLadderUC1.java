97a3248917ef44ed3cc5f2441246d2be4b2a87d8
Rushi5078 <88040056+Rushi5078@users.noreply.github.com>
2021-09-02 18:39:48 +0530

    Add: initial commit

---
+++ SnakeLadderUC1.java
@@ -0,0 +1,16 @@
+package game;
+
+public class SnakeLadder {
+
+	public static void main(String[] args) {
+
+		// variable declare
+		int player=1;
+
+		
+		int position=0;
+
+		System.out.println("Player  position = "+position);
+	}
+	
+}
